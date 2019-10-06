d6903c604c29916052775ff29ad6399da61972a4
Kharoh <Gaulu03@gmail.com>
2019-10-06 19:22:58 +0200

    Updates

---
+++ precache-manifest.ab2943c3477abf5d3b9310390d699d29.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d115921e715d25851b0519717d537271",
+    "url": "/Binary-Number-Image/index.html"
+  },
+  {
+    "revision": "d487d5ed7f2eb1168eef",
+    "url": "/Binary-Number-Image/static/css/main.147b028b.chunk.css"
+  },
+  {
+    "revision": "f80be28a59b85813f18e",
+    "url": "/Binary-Number-Image/static/js/2.dfbf1714.chunk.js"
+  },
+  {
+    "revision": "d487d5ed7f2eb1168eef",
+    "url": "/Binary-Number-Image/static/js/main.e4a1cabe.chunk.js"
+  },
+  {
+    "revision": "929bbd2edbcd7e4b3e3c",
+    "url": "/Binary-Number-Image/static/js/runtime~main.e459eede.js"
+  }
+]);
